c2d6cd43387e5e4c72fd71d836780a9d2f4c95a3
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-03-12 09:52:37 -0400

    Bugs

---
+++ app/src/main/java/com/example/proyectofinal/APIs/Movie.java
@@ -0,0 +1,4 @@
+package com.example.proyectofinal.APIs;
+
+public class Movie {
+}
--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt1 = findViewById(R.id.txt1);
 
 
     }
